9bc13bc03c41081f8af8b076b6ca24003479d294
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-06-09 16:40:06 +0200

    Add files via upload

---
+++ 03.3.SoftUniBarIncome.js
@@ -0,0 +1,27 @@
+function solve(input) {
+let namePattern = /%([A-Z][a-z]*)%.*?<(\w+)>.*?\|(\d+)\|.*?(-?\d+(?:\.\d+)?)\$/;
+let total = 0;
+
+while( input[0] != 'end of shift') {
+    let line = input.shift();
+
+    let match = namePattern.exec(line);
+
+    if(match != null) {
+        let [_, name, product, qty, price] = match;
+        qty = Number(qty);
+        price = Number(price);
+
+        total += qty * price
+        console.log(`${name}: ${product} - ${(qty * price).toFixed(2)}`);
+    }
+}
+
+console.log(`Total income: ${total.toFixed(2)}`)
+
+
+}
+solve(['%George%<Croissant>|2|10.3$',
+'%Peter%<Gum>|1|1.3$',
+'%Maria%<Cola>|1|2.4$',
+'end of shift']);
